b59cf158f39041ae039f814a9df70a35ed3cc674
Ashwin3207 <amathura01@gmail.com>
2024-04-18 23:52:13 +0530

    password gen

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+ 
+  
+const [counter,setCounter]= useState(0);
+
+const addValue = () =>
+{const butt1 =document.getElementById('but1');
+if(counter===100)
+{
+  butt1.display=none;
+}
+  setCounter((prevCounter=>prevCounter+1)); // callback like this doesnt make batching 
+}
+
+
+const removeValue= ()=>
+{
+  setCounter(counter-1);
+}
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <h1>React Course {counter}</h1>
+   <h2>Counter Value : {counter}</h2>
+   <button onClick={addValue} id ="but1">Add Value</button>
+   <button onClick={removeValue}>Remove Value </button>
+   <p>footer : {counter}</p>
+   </>
   )
 }
 
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-3xl bg-green-500 p-3 rounded'>Vite with Tailwind</h1>
+     <Card/>
+    </>
+  )
+}
+
+export default App 
