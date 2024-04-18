85da8a2c05917eb287dcaf6738aa7d6246dfe3a9
Ashwin3207 <amathura01@gmail.com>
2024-04-18 12:12:56 +0530

    2 projs

---
+++ customReact/customReact.js
@@ -0,0 +1,18 @@
+function customRender(reactElement, container)
+{
+    document.createElement()
+}
+
+const reactElement
+={
+    type : 'a',
+    props : 
+    {
+        href : "https://google.com",
+        target: '_blank'
+    },
+    children : 'Click to go to google'
+}
+const mainContainer = document.querySelector('#root');
+
+customRender(reactElement,mainContainer)
