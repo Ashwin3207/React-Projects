581e7d41ac071a4aa35264e23e079d869dc22ea2
Ashwin3207 <amathura01@gmail.com>
2024-04-19 16:24:46 +0530

    input box for currency change

---
+++ 06currencyconvertor/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'>
+
+    </div>
+     <h1 className='bg-green-600 text-black'>Test</h1>
+    </>
+  )
+}
+
+export default App
